90252d203b296cac4c9fbe62350be01ee9f7496f
noahsmiley <noahsmiley123@gmail.com>
2020-12-17 00:12:00 -0600

    Added Shuffle Method, and def main() for testing.

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,7 @@
+import random
 class Cards():
 
     def __init__(self):
-
         self.deck=[]
         self.suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
         self.faces =[['Ace',1],['Two',2],['Three',3],['Four',4],
@@ -14,12 +14,17 @@ class Cards():
                 x = (suit,face)
                 self.deck.append(x)
 
+    def shuffle(self):
+        random.shuffle(self.deck)
+
     def __str__(self):
         strings=[]
         for i in range(len(self.deck)):
             strings.append(f" The {self.deck[i][1][0]} of {self.deck[i][0]}")
         return '\n'.join(strings)
 
-newCardDeck = Cards()
-
-print(newCardDeck)
+def main():
+    newCardDeck = Cards()
+    newCardDeck.shuffle()
+    print(newCardDeck)
+main()
