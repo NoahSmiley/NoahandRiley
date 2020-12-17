5a5db6f820da4b5cb461f39bdcf8c01def232fbe
noahsmiley <noahsmiley123@gmail.com>
2020-12-17 00:02:20 -0600

    Print for the deck added (dunder str method).

--- blackjack.py
+++ blackjack.py
@@ -2,19 +2,24 @@ class Cards():
 
     def __init__(self):
 
-        suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
-        faces =[['Ace',1],['Two',2],['Three',3],['Four',4],
-               ['Five',5],['Six',6],['Seven',7],['Eight',8],
-               ['Nine',9],['Ten',10],['Jack',11],['Queen',12],
-               ['King',13]]
-        deck=[]
-        #Making
-        for suit in suits:
-            for face in faces:
-                x = (suit,face)
-                deck.append(x)
+        self.deck=[]
+        self.suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
+        self.faces =[['Ace',1],['Two',2],['Three',3],['Four',4],
+                    ['Five',5],['Six',6],['Seven',7],['Eight',8],
+                    ['Nine',9],['Ten',10],['Jack',11],['Queen',12],
+                    ['King',13]]
 
-        for i in range(len(deck)):
-            print(f" The {deck[i][1][0]} of {deck[i][0]}")
+        for suit in self.suits:
+            for face in self.faces:
+                x = (suit,face)
+                self.deck.append(x)
+
+    def __str__(self):
+        strings=[]
+        for i in range(len(self.deck)):
+            strings.append(f" The {self.deck[i][1][0]} of {self.deck[i][0]}")
+        return '\n'.join(strings)
 
 newCardDeck = Cards()
+
+print(newCardDeck)
